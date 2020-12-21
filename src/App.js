542ecfb6d45185086ff8de83fdbea54c1796eba6
dwisatriow <31796624+dwisatriow@users.noreply.github.com>
2020-12-21 22:16:41 +0700

    Added logic
    Class based component used for App and added program logic to render markdown text to html

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import marked from 'marked';
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 
-function App() {
-  return (
-      <h3>Markdown Previewer</h3>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mdText: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ mdText: event.target.value});
+  }
+
+  render() {
+    const htmlText = marked(this.state.mdText);
+
+    return (
+      <div>
+        <form>
+          <label>Editor</label><br />
+          <textarea id="editor" value={this.state.mdText} onChange={this.handleChange} />
+        </form>
+        <div id="preview">
+          {ReactHtmlParser(htmlText)}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
