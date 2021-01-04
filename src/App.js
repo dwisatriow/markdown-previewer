73106892add95b6bf02ce3dd8f099a8009b96719
dwisatriow <31796624+dwisatriow@users.noreply.github.com>
2021-01-05 02:45:23 +0700

    Added some styles and responsiveness
    Bootstrap 4 still used

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.scss';
 import Header from './components/Header.js';
 import Editor from './components/Editor.js';
 import Footer from './components/Footer.js';
+import placeholderText from './placeholderText.js';
 
 class App extends React.Component {
   constructor(props) {
@@ -16,6 +17,14 @@ class App extends React.Component {
 
   handleChange(event) {
     this.setState({ md: event.target.value});
+  }
+
+  componentDidMount() {
+    let formattedText = "";
+    for (const line in placeholderText) {
+      formattedText = formattedText.concat(placeholderText[line]+"\n");
+    }
+    this.setState({ md: formattedText});
   }
 
   render() {
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -9,14 +9,18 @@ function Editor(props) {
   return (
     <div className="container">
       <div id="editor-previewer" className="row no-gutters">
-        <div id="editor-container" className="col-sm-12 col-md-6 mb-sm-4 mb-md-0 mr-sm-0 mr-md-3">
-          <h2>Editor</h2>
-          <textarea id="editor" value={props.md} onChange={props.handleChange} />
+        <div id="container-wrapper" className="col-md-12 col-lg-6 px-4 px-sm-0 pr-md-0 pr-lg-4 mb-4 mb-lg-0">
+          <div id="editor-container">
+            <h2>Editor</h2>
+            <textarea id="editor" value={props.md} onChange={props.handleChange} />
+          </div>
         </div>
-        <div id="preview-container" className="col-sm-12 col-md-6 ml-sm-0 ml-md-3">
-          <h2>Preview</h2>
-          <div id="preview">
-            {ReactHtmlParser(html)}
+        <div id="container-wrapper" className="col-md-12 col-lg-6 px-4 px-sm-0 pl-md-0 pl-lg-4">
+          <div id="preview-container">
+            <h2>Preview</h2>
+            <div id="preview">
+              {ReactHtmlParser(html)}
+            </div>
           </div>
         </div>
       </div>
