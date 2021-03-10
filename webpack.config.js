1d65926a1f956faa2f912f98569faf216bac38b7
dwisatriow <31796624+dwisatriow@users.noreply.github.com>
2021-03-10 23:23:16 +0700

    Moved build folder to docs for github pages source

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'docs'),
     filename: 'bundle.js',
   },
   resolve: {
